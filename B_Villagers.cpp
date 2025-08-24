037b0bf438fbb5d7c73c206f04997990d0262dd5
Aryan-Vash <147941821+Aryan-Vash@users.noreply.github.com>
2025-08-24 21:38:09 +0530

    Add files via upload

---
+++ B_Villagers.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void fnc() {
+    ll n; cin >> n;
+    vector <int> v(n);
+    for (int i=0;i<n;i++) cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    long long ans = 0;
+    for (int i=n-1;i>=0;i-=2) {
+        ans += v[i];
+    }
+    cout << ans << endl;
+}
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int lines;
+    lines = 1;
+    cin >> lines;
+    while (lines--) {
+        fnc();
+    }
+    return 0;
+}
