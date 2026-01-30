ef80592eb1e5ce2fb9b180b552559d8fd4953422
Aryan Vashishtha <147941821+Aryan-Vash@users.noreply.github.com>
2026-01-31 04:42:52 +0530

    Add files via upload

---
+++ A_Perfect_Root.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    
+    for (int i=1;i<=n;i++) {
+        cout << i << " ";
+    }
+    cout << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
